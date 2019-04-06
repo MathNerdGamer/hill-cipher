c07472b5e65e26d02be8d6c79fddf05d674574ae
Math Nerd <mathnerd@anothermathblog.com>
2019-04-06 01:25:56 +0000

    Removed inverse_of() function, replaced with definition of newly declared inverse() member function of matrix_t for the hill_key type.

--- hill_cipher.h
+++ hill_cipher.h
@@ -21,7 +21,6 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-
 #pragma once
 #ifndef MATH_NERD_HILL_CIPHER_H
 #define MATH_NERD_HILL_CIPHER_H
@@ -91,12 +90,6 @@ namespace math_nerd
                 return std::distance(std::begin(ch_table), std::find(std::begin(ch_table), std::end(ch_table), c));
             }
 
-            /** \fn hill_key<size> inverse_of(hill_key<size> key)
-                \brief Returns the inverse of the size x size Hill Cipher key matrix using Gauss-Jordan Elimination for sizes > 2.
-                       For size = 2, assuming the determinant \f$ ad-bc\neq 0 \f$, we have
-                       \f$\left[\begin{array}{cc} a & b\\ c & d \end{array}\right]^{-1}=\frac{1}{ad-bc}\left[\begin{array}{cc} d & -b\\ -c & a \end{array}\right]\f$
-             */
-            hill_key inverse_of(hill_key key);
         } // namespace impl_details
 
         /** \fn std::string encrypt(hill_key<size> key, std::string pt)
@@ -157,148 +150,14 @@ namespace math_nerd
          */
         std::string decrypt(hill_key key, std::string const &ct)
         {
-            return encrypt(impl_details::inverse_of(key), ct);
-        }
-
-        hill_key impl_details::inverse_of(hill_key key)
-        {
-            std::int64_t size = key.row_count();
-            hill_key dec_key{ size, size };
-    
-            if ( size == 2 )
-            {
-                // Check if determinant is 0.
-                if( key[0][0] * key[1][1] == key[0][1] * key[1][0] )
-                {
-                    throw std::invalid_argument("The matrix is not invertible.\n");
-                }
-
-                // Calculate and hold determinant.
-                z97 det = key[0][0] * key[1][1] - key[0][1] * key[1][0];
-
-                // This gives the inverse matrix for 2x2 matrices.
-                dec_key[0][0] =  key[1][1] / det;
-                dec_key[0][1] = -key[0][1] / det;
-                dec_key[1][0] = -key[1][0] / det;
-                dec_key[1][1] =  key[0][0] / det;
-
-                return dec_key;
-            }
-            else
-            {
-                // Creating identity matrix.
-                // dec_key acts as the augmented portion of the key matrix in the Gauss-Jordan Elimination algorithm.
-                for( auto i = 0; i < size; ++i )
-                {
-                    for( auto j = 0; j < size; ++j )
-                    {
-                        if( j == i )
-                        {
-                            dec_key[i][j] = 1;
-                        }
-                        else
-                        {
-                            dec_key[i][j] = 0;
-                        }
-                    }
-                }
-
-                for( auto i = 0; i < size; ++i )
-                {
-                    auto max_element = key[i][i].value();
-                    auto max_row = i;
-                    for( auto k = i + 1; k < size; ++k )
-                    {
-                        if( key[k][i].value() > max_element )
-                        {
-                            max_element = key[k][i].value();
-                            max_row = k;
-                        }
-                    }
-
-                    // Swap maximum element in pivot positions to their corresponding rows
-                    if( i < max_row )
-                    {
-                        for( auto j = 0u; j < size; ++j )
-                        {
-                            auto tmp = key[max_row][j];
-                            key[max_row][j] = key[i][j];
-                            key[i][j] = tmp;
-
-                            tmp = dec_key[max_row][j];
-                            dec_key[max_row][j] = dec_key[i][j];
-                            dec_key[i][j] = tmp;
-                        }
-                    }
-
-                    // Make all elements in the rows below the pivot zero using row operations.
-                    for( auto k = i + 1; k < size; ++k )
-                    {
-                        if( key[i][i] == 0 )
-                        {
-                            throw std::invalid_argument("The matrix is not invertible.\n");
-                        }
-
-                        auto d = key[k][i] / key[i][i];
-
-                        for( auto j = 0; j < size; ++j )
-                        {
-
-                            key[k][j] -= d * key[i][j];
-                            dec_key[k][j] -= d * dec_key[i][j];
-                        }
-
-                    }
-                }
-
-                // Turn key matrix into identity matrix using row operations.
-                // Copy these same row operations to dec_key to find the inverse matrix.
-                for( auto i = size - 1; i >= 0; --i )
-                {
-                    if( key[i][i] == 0 )
-                    {
-                        throw std::invalid_argument("The matrix is not invertible.\n");
-                    }
-
-                    for( auto j = 0; j < size; ++j )
-                    {
-                        dec_key[i][j] /= key[i][i];
-                    }
-
-                    key[i][i] = 1;
-
-                    if( i != 0 )
-                    {
-                        for( auto row = i - 1; row >= 0; --row )
-                        {
-                            for( auto column = 0; column < size; ++column )
-                            {
-                                dec_key[row][column] -= dec_key[i][column] * key[row][i];
-                            }
-
-                            key[row][i] = 0;
-
-                            if( row == 0 )
-                            {
-                                break;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        break;
-                    }
-                }
-
-                return dec_key;
-            }
+            return encrypt(key.inverse(), ct);
         }
 
         bool is_valid_key(hill_key key)
         {
             try
             {
-                impl_details::inverse_of(key);
+                key.inverse();
             }
             catch( std::invalid_argument const & )
             {
@@ -309,6 +168,140 @@ namespace math_nerd
         }
 
     } // namespace hill_cipher
+
+    template<>
+    hill_cipher::hill_key hill_cipher::hill_key::inverse() const
+    {
+        auto size = row_count();
+
+        auto key{ *this };
+        hill_cipher::hill_key dec_key{ size, size };
+
+        if ( size == 2 )
+        {
+            // Check if determinant is 0.
+            if( mat[0][0] * mat[1][1] == mat[0][1] * mat[1][0] )
+            {
+                throw std::invalid_argument("The matrix is not invertible.\n");
+            }
+
+            // Calculate and hold determinant.
+            hill_cipher::z97 det = key[0][0] * key[1][1] - key[0][1] * key[1][0];
+
+            // This gives the inverse matrix for 2x2 matrices.
+            dec_key[0][0] =  key[1][1] / det;
+            dec_key[0][1] = -key[0][1] / det;
+            dec_key[1][0] = -key[1][0] / det;
+            dec_key[1][1] =  key[0][0] / det;
+        }
+        else
+        {
+            // Creating identity matrix.
+            // dec_key acts as the augmented portion of the key matrix in the Gauss-Jordan Elimination algorithm.
+            for( auto i = 0; i < size; ++i )
+            {
+                for( auto j = 0; j < size; ++j )
+                {
+                    if( j == i )
+                    {
+                        dec_key[i][j] = 1;
+                    }
+                    else
+                    {
+                        dec_key[i][j] = 0;
+                    }
+                }
+            }
+
+            for( auto i = 0; i < size; ++i )
+            {
+                auto max_element = key[i][i].value();
+                auto max_row = i;
+                for( auto k = i + 1; k < size; ++k )
+                {
+                    if( key[k][i].value() > max_element )
+                    {
+                        max_element = key[k][i].value();
+                        max_row = k;
+                    }
+                }
+
+                // Swap maximum element in pivot positions to their corresponding rows
+                if( i < max_row )
+                {
+                    for( auto j = 0u; j < size; ++j )
+                    {
+                        auto tmp = key[max_row][j];
+                        key[max_row][j] = key[i][j];
+                        key[i][j] = tmp;
+
+                        tmp = dec_key[max_row][j];
+                        dec_key[max_row][j] = dec_key[i][j];
+                        dec_key[i][j] = tmp;
+                    }
+                }
+
+                // Make all elements in the rows below the pivot zero using row operations.
+                for( auto k = i + 1; k < size; ++k )
+                {
+                    if( key[i][i] == 0 )
+                    {
+                        throw std::invalid_argument("The matrix is not invertible.\n");
+                    }
+
+                    auto d = key[k][i] / key[i][i];
+
+                    for( auto j = 0; j < size; ++j )
+                    {
+                        key[k][j] -= d * key[i][j];
+                        dec_key[k][j] -= d * dec_key[i][j];
+                    }
+
+                }
+            }
+
+            // Turn key matrix into identity matrix using row operations.
+            // Copy these same row operations to dec_key to find the inverse matrix.
+            for( auto i = size - 1; i >= 0; --i )
+            {
+                if( key[i][i] == 0 )
+                {
+                    throw std::invalid_argument("The matrix is not invertible.\n");
+                }
+
+                for( auto j = 0; j < size; ++j )
+                {
+                    dec_key[i][j] /= key[i][i];
+                }
+
+                key[i][i] = 1;
+
+                if( i != 0 )
+                {
+                    for( auto row = i - 1; row >= 0; --row )
+                    {
+                        for( auto column = 0; column < size; ++column )
+                        {
+                            dec_key[row][column] -= dec_key[i][column] * key[row][i];
+                        }
+
+                        key[row][i] = 0;
+
+                        if( row == 0 )
+                        {
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        return dec_key;
+    }
 } // namespace math_nerd
 #endif // MATH_NERD_HILL_CIPHER
 
