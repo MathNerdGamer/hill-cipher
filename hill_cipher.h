930a1d5a18507d1dc66905c196025a83cbc0a9e6
Math Nerd <mathnerd@anothermathblog.com>
2020-09-01 08:05:50 +0000

    Fixed specialization after instantiation issue.

--- hill_cipher.h
+++ hill_cipher.h
@@ -57,118 +57,8 @@ namespace math_nerd
         /** \name Hill Cipher message block
          */
         using msg_block = matrix_t::matrix_t<z97>;
+    }
 
-        /** \namespace math_nerd::hill_cipher::impl_details
-            \brief Contains implementation details.
-         */
-        namespace impl_details
-        {
-            /** \property ch_table
-                \brief The character table.
-             */
-            constexpr std::array<char, 97> ch_table =
-            {
-                'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
-                'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
-                '0','1','2','3','4','5','6','7','8','9',' ','~','-','=','!','@','#','$','%','^','&','*','(',')','_','+',
-                '[',']',';','\'',',','.','/','{','}',':','"','<','>','?','`', '\\', '|', '\t', '\n'
-            };
-
-            /** \fn constexpr char z97_to_char(z97 num)
-                \brief Returns the character assigned to that integer modulo 97.
-             */
-            char z97_to_char(z97 num)
-            {
-                return ch_table[static_cast<std::size_t>(num.value())];
-            }
-
-            /** \fn constexpr z97 char_to_z97(char c)
-                \brief Returns the integer modulo 97 assigned to that character.
-             */
-            z97 char_to_z97(char c)
-            {
-                return std::distance(std::begin(ch_table), std::find(std::begin(ch_table), std::end(ch_table), c));
-            }
-
-        } // namespace impl_details
-
-        /** \fn std::string encrypt(hill_key key, std::string pt)
-            \brief Encrypts plaintext string using the key by breaking the string into blocks the same size as the matrix key and multiplying by the key.
-         */
-        std::string encrypt(hill_key key, std::string pt)
-        {
-            std::int64_t size = key.row_count();
-
-            std::vector<msg_block> plain_text_blocks;
-            std::vector<msg_block> cipher_text_blocks;
-
-            using namespace impl_details;
-
-            // Pad plaintext to make its length a multiple of size
-            while( (pt.length() % size) != 0 )
-            {
-                pt += ' ';
-            }
-
-            // Take each `size` characters as a message block, put in vector of plaintext blocks.
-            for( auto i = 0u; i < pt.length(); i += static_cast<unsigned>(size) )
-            {
-                msg_block tmp{ size, 1 };
-
-                for( auto j = 0; j < size; ++j )
-                {
-                    tmp[j][0] = char_to_z97(pt[i + j]);
-                }
-
-                plain_text_blocks.push_back(tmp);
-            }
-
-            // Multiply each block with the key matrix and put in vector of ciphertext blocks.
-            for( auto it = plain_text_blocks.begin(); it != plain_text_blocks.end(); ++it )
-            {
-                cipher_text_blocks.push_back(key * (*it));
-            }
-
-            // Create an empty ciphertext string.
-            std::string ct{ "" };
-
-            // Convert each ciphertext block into the corresponding characters.
-            for( auto it = cipher_text_blocks.begin(); it != cipher_text_blocks.end(); ++it )
-            {
-                for( auto j = 0; j < size; ++j )
-                {
-                    ct += z97_to_char((*it)[j][0]);
-                }
-            }
-
-            // Return the ciphertext string.
-            return ct;
-        }
-
-        /** \fn std::string decrypt(hill_key key, std::string const &ct)
-            \brief Decrypts ciphertext by calling the encrypt function with the inverse matrix.
-         */
-        std::string decrypt(hill_key key, std::string const &ct)
-        {
-            return encrypt(key.inverse(), ct);
-        }
-
-        bool is_valid_key(hill_key key)
-        {
-            try
-            {
-                key.inverse();
-            }
-            catch( std::invalid_argument const & )
-            {
-                return false;
-            }
-
-            return true;
-        }
-
-    } // namespace hill_cipher
-    
     template<>
     hill_cipher::hill_key hill_cipher::hill_key::inverse() const
     {
@@ -302,6 +192,119 @@ namespace math_nerd
 
         return dec_key;
     }
+
+    namespace hill_cipher
+    {
+        /** \namespace math_nerd::hill_cipher::impl_details
+            \brief Contains implementation details.
+         */
+        namespace impl_details
+        {
+            /** \property ch_table
+                \brief The character table.
+             */
+            constexpr std::array<char, 97> ch_table =
+            {
+                'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
+                'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+                '0','1','2','3','4','5','6','7','8','9',' ','~','-','=','!','@','#','$','%','^','&','*','(',')','_','+',
+                '[',']',';','\'',',','.','/','{','}',':','"','<','>','?','`', '\\', '|', '\t', '\n'
+            };
+
+            /** \fn constexpr char z97_to_char(z97 num)
+                \brief Returns the character assigned to that integer modulo 97.
+             */
+            char z97_to_char(z97 num)
+            {
+                return ch_table[static_cast<std::size_t>(num.value())];
+            }
+
+            /** \fn constexpr z97 char_to_z97(char c)
+                \brief Returns the integer modulo 97 assigned to that character.
+             */
+            z97 char_to_z97(char c)
+            {
+                return std::distance(std::begin(ch_table), std::find(std::begin(ch_table), std::end(ch_table), c));
+            }
+
+        } // namespace impl_details
+
+        /** \fn std::string encrypt(hill_key key, std::string pt)
+            \brief Encrypts plaintext string using the key by breaking the string into blocks the same size as the matrix key and multiplying by the key.
+         */
+        std::string encrypt(hill_key key, std::string pt)
+        {
+            std::int64_t size = key.row_count();
+
+            std::vector<msg_block> plain_text_blocks;
+            std::vector<msg_block> cipher_text_blocks;
+
+            using namespace impl_details;
+
+            // Pad plaintext to make its length a multiple of size
+            while( (pt.length() % size) != 0 )
+            {
+                pt += ' ';
+            }
+
+            // Take each `size` characters as a message block, put in vector of plaintext blocks.
+            for( auto i = 0u; i < pt.length(); i += static_cast<unsigned>(size) )
+            {
+                msg_block tmp{ size, 1 };
+
+                for( auto j = 0; j < size; ++j )
+                {
+                    tmp[j][0] = char_to_z97(pt[i + j]);
+                }
+
+                plain_text_blocks.push_back(tmp);
+            }
+
+            // Multiply each block with the key matrix and put in vector of ciphertext blocks.
+            for( auto it = plain_text_blocks.begin(); it != plain_text_blocks.end(); ++it )
+            {
+                cipher_text_blocks.push_back(key * (*it));
+            }
+
+            // Create an empty ciphertext string.
+            std::string ct{ "" };
+
+            // Convert each ciphertext block into the corresponding characters.
+            for( auto it = cipher_text_blocks.begin(); it != cipher_text_blocks.end(); ++it )
+            {
+                for( auto j = 0; j < size; ++j )
+                {
+                    ct += z97_to_char((*it)[j][0]);
+                }
+            }
+
+            // Return the ciphertext string.
+            return ct;
+        }
+
+        /** \fn std::string decrypt(hill_key key, std::string const &ct)
+            \brief Decrypts ciphertext by calling the encrypt function with the inverse matrix.
+         */
+        std::string decrypt(hill_key key, std::string const &ct)
+        {
+            return encrypt(key.inverse(), ct);
+        }
+
+        bool is_valid_key(hill_key key)
+        {
+            try
+            {
+                key.inverse();
+            }
+            catch( std::invalid_argument const & )
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+    } // namespace hill_cipher
 
 } // namespace math_nerd
 #endif // MATH_NERD_HILL_CIPHER
